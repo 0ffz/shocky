467e32682716daeb9120cb328132cff3439660b3
Danielle Voznyy <dan.voznyy@gmail.com>
2024-11-09 01:21:12 -0500

    feat: Swap to standalone tailwindCSS fix: Markdown incorrectly embedding code blocks because of indentation feat: Auto reload script when using `serve`

--- shocky-generator/src/main/kotlin/me/dvyy/shocky/Markdown.kt
+++ shocky-generator/src/main/kotlin/me/dvyy/shocky/Markdown.kt
@@ -1,19 +1,17 @@
 package me.dvyy.shocky
 
-import kotlinx.html.FlowContent
-import kotlinx.html.HTMLTag
-import kotlinx.html.unsafe
-import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import kotlinx.html.*
+import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
 import org.intellij.markdown.parser.MarkdownParser
 
-val flavour = CommonMarkFlavourDescriptor()
+val flavour = GFMFlavourDescriptor()
 
 fun FlowContent.markdown(src: String) {
     val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(src)
     val html = HtmlGenerator(src, parsedTree, flavour).generateHtml()
-    (this as? HTMLTag)?.unsafe {
-        +html
+    div {
+        unsafe { +html }
     }
 }
 
--- shocky-generator/src/main/kotlin/me/dvyy/shocky/Shocky.kt
+++ shocky-generator/src/main/kotlin/me/dvyy/shocky/Shocky.kt
@@ -1,13 +1,20 @@
 package me.dvyy.shocky
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.html.dom.append
 import kotlinx.html.dom.write
+import kotlinx.html.script
+import me.dvyy.shocky.dev.autoReloadScript
 import java.nio.file.Path
 import kotlin.io.path.*
 import kotlin.time.measureTime
@@ -22,24 +29,30 @@ class Shocky(
     val dest: Path,
     val route: SiteRouting,
     val assets: List<Path> = listOf(),
-    val devMode: Boolean = System.getenv("DEVELOPMENT") == "true",
     val useTailwind: Boolean = true,
+    val port: Int = 8080,
 ) {
     @OptIn(ExperimentalPathApi::class)
-    fun generate() {
+    suspend fun generate(devMode: Boolean) = withContext(Dispatchers.IO) {
         measureTime {
             if (!devMode) dest.deleteRecursively()
             dest.createDirectories()
         }.let { println("Cleared output in: $it") }
-        if (useTailwind) runCommand("npx", "tailwindcss", "-o", "out/assets/tailwind/styles.css", "--minify")
-        measureTime {
-            assets
-                .forEach { it.copyToRecursively(dest / it.name, followLinks = false, overwrite = true) }
-        }.let { println("Copied extra inputs in: $it") }
-        measureTime { generateDocuments() }.let { println("Generated html files in: $it") }
+
+        if (useTailwind) {
+            launch {
+                runCommand("./build/tailwindcss/tailwindcss", "-o", "out/assets/tailwind/styles.css", "--minify")
+            }
+        }
+        launch {
+            measureTime {
+                assets.forEach { it.copyToRecursively(dest / it.name, followLinks = false, overwrite = true) }
+            }.let { println("Copied extra inputs in: $it") }
+            println("Generated html files in: ${measureTime { generateDocuments(devMode) }}")
+        }
     }
 
-    fun generateDocuments() {
+    fun generateDocuments(devMode: Boolean) {
         route.assets.forEach {
             val dest = dest / it.relativeTo(route.route)
             dest.createParentDirectories()
@@ -50,17 +63,26 @@ class Shocky(
             path.createParentDirectories().also { if (it.notExists()) it.createFile() }
                 .writer()
                 .use { writer ->
-                    document.page.html?.let { writer.write(it) }
+                    document.page.html
+                        ?.apply {
+                            if (devMode) getElementsByTagName("head").item(0)?.append {
+                                script(src = "/assets/scripts/autoreload.js") {
+                                    defer = true
+                                }
+                            }
+                        }
+                        ?.let { writer.write(it, prettyPrint = false) }
                 }
         }
     }
 
     suspend fun run(args: Array<String>) {
-        val type = args.firstOrNull()
+        val type = args.getOrNull(0)
+        val devMode = System.getenv("DEVELOPMENT") == "true"
         when (type) {
-            "build" -> generate()
+            "build" -> generate(devMode = devMode)
             "serve" -> {
-                generate()
+                generate(devMode = devMode)
                 startServerAndWatch()
             }
 
@@ -75,13 +97,20 @@ class Shocky(
     ) {
         embeddedServer(
             Netty,
-            port = 8080,
+            port = port,
             host = "localhost",
         ) {
             routing {
+                get("/assets/scripts/autoreload.js") {
+                    call.respondText(
+                        autoReloadScript(),
+                        contentType = ContentType.Text.JavaScript
+                    )
+                }
                 staticFiles("/", dest.toFile()) {
                     extensions("html")
                 }
+
             }
             configure(this)
         }.start(wait = true)
@@ -93,18 +122,13 @@ class Shocky(
         }
 
         launch {
-            ProcessBuilder("./gradlew", "-t", "generate").apply {
+            ProcessBuilder("./gradlew", "-t", "generate", "--no-daemon").apply {
                 environment()["JAVA_HOME"] = System.getProperty("java.home")
                 environment()["DEVELOPMENT"] = "true"
                 redirectInput(ProcessBuilder.Redirect.INHERIT)
                 redirectOutput(ProcessBuilder.Redirect.INHERIT)
                 redirectError(ProcessBuilder.Redirect.INHERIT)
-            }.start()/*.inputStream.bufferedReader().lineSequence().forEach {
-            val line = it.lowercase()
-            if(listOf("rebuilding", "build").any { line.startsWith(it) }) {
-                println(it)
-            }
-        }*/
+            }.start()
         }
     }
 }
