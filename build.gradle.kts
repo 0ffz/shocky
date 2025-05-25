98762eb07f968f12e45d9a2cebd59c742a371bf0
Danielle Voznyy <dan.voznyy@gmail.com>
2025-05-25 02:22:47 -0400

    chore: Bump dependencies, make markdown tag infix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("jvm") version "2.2.0-RC"
+    kotlin("plugin.serialization") version "2.2.0-RC"
     `maven-publish`
 }
 
 group = "me.dvyy"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
@@ -13,18 +13,18 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlinx:kotlinx-html-jvm:0.11.0")
+    api("org.jetbrains.kotlinx:kotlinx-html-jvm:0.12.0")
     api("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    api("com.charleskorn.kaml:kaml:0.61.0")
+    api("com.charleskorn.kaml:kaml:0.78.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines:0.19.2")
-    implementation("io.ktor:ktor-server-core:3.0.0")
-    implementation("io.ktor:ktor-server-cio:3.0.0")
-    implementation("io.ktor:ktor-server-websockets:3.0.0")
-    implementation("io.ktor:ktor-server-html-builder:3.0.0")
+    implementation("io.ktor:ktor-server-core:3.1.3")
+    implementation("io.ktor:ktor-server-cio:3.1.3")
+    implementation("io.ktor:ktor-server-websockets:3.1.3")
+    implementation("io.ktor:ktor-server-html-builder:3.1.3")
     implementation("org.jetbrains:markdown:0.7.3")
-    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("io.methvin:directory-watcher:0.18.0")
 }
 
--- src/me/dvyy/shocky/Markdown.kt
+++ src/me/dvyy/shocky/Markdown.kt
@@ -1,7 +1,6 @@
 package me.dvyy.shocky
 
 import kotlinx.html.*
-import kotlinx.html.stream.createHTML
 import org.intellij.lang.annotations.Language
 import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
@@ -9,12 +8,16 @@ import org.intellij.markdown.parser.MarkdownParser
 
 val flavour = GFMFlavourDescriptor()
 
-fun Tag.markdown(@Language("markdown") src: String) {
+infix fun Tag.markdown(@Language("markdown") src: String) {
     val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(src)
     val html = HtmlGenerator(src, parsedTree, flavour).generateHtml()
 
-    when(this) {
+    when (this) {
         is HTMLTag -> unsafe { +html }
         is FlowContent -> div { unsafe { +html } }
     }
+}
+
+infix fun Tag.md(@Language("markdown") src: String) {
+    markdown(src)
 }
